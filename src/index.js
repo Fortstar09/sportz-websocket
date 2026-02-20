a0cf4f96c4496d972755e2531487f4c6ba293551
Fortstar09 <ogunyafort@gmail.com>
2026-02-20 14:49:47 +0100

    implement get and post matches endpoint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { matchRouter } from "./routes/matches.js";
 const app = express();
 const PORT = 3000;
 
@@ -7,6 +8,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello from Express Server!");
 });
+
+app.use("/matches", matchRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
